90f80840e5be0f83d0d040c48e6b808f92237f86
DarthChurros <tnarayan00@stuy.edu>
2019-04-02 11:19:08 -0400

    bug fix, basic tester

--- Radix.java
+++ Radix.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Radix {
   public static void radixsort(int[] data) {
     @SuppressWarnings({"unchecked", "rawtypes"})
@@ -9,12 +11,19 @@ public class Radix {
     while (buckets[9].size() + buckets[10].size() < data.length) {
       while (list.size() > 0) {
         int item = list.removeFront();
-        int digit = (item/(int)Math.pow(10,place))%10;
+        int digit = (item/(int)Math.pow(10,place++))%10;
         if (item < 0) buckets[9-digit].add(item);
         else buckets[digit+10].add(item);
       }
     }
     int i = 0;
     while (list.size() > 0) data[i++] = list.removeFront();
+  }
+
+  public static void main(String[] args) {
+    int[] data = new int[]{103, 45, 241, 861, 359, 175, 920, 4};
+    System.out.println("unsorted: "+Arrays.toString(data));
+    radixsort(data);
+    System.out.println("sorted: "+Arrays.toString(data));
   }
 }
