6e7855fb10b601b7a88f78719909c14e96ccab15
DarthChurros <tnarayan00@stuy.edu>
2019-04-02 13:02:03 -0400

    fixed digit calculation

--- Radix.java
+++ Radix.java
@@ -15,7 +15,7 @@ public class Radix {
     while (place <= maxDigits) {
       while (list.size() > 0) {
         int item = list.removeFront();
-        int digit = (item/(int)Math.pow(10,place))%10;
+        int digit = (Math.abs(item)/(int)Math.pow(10,place))%10;
         if (item < 0) buckets[9-digit].add(item);
         else buckets[digit+10].add(item);
       }
@@ -26,14 +26,14 @@ public class Radix {
         buckets[i] = new MyLinkedList<Integer>();
       }
       place++;
-      //System.out.println(list);
+      //System.out.println("list: "+list);
     }
     int i = 0;
     while (list.size() > 0) data[i++] = list.removeFront();
   }
 
   public static void main(String[] args) {
-    int[] data = new int[]{103, 45, 241, 861, 359, 175, 920, 4};
+    int[] data = new int[]{103, 45, -1, 241, 861, 359, 175, 920, 4};
     System.out.println("unsorted: "+Arrays.toString(data));
     radixsort(data);
     System.out.println("sorted: "+Arrays.toString(data));
